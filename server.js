ad29c5d5b0ae693cd2901346a81b9d0250f45129
rileyeh <rhatchdev@gmail.com>
2021-06-10 13:33:41 -0700

    changes for serving files

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const path = require('path')
 
 const app = express()
 
+// app.use('/styles', express.static(path.join(__dirname, '/public/styles.css')))
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('/styles', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/styles.css'))
+})
+
+app.get('/cat', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/tally.png'))
 })
 
 const port = process.env.PORT || 6050
